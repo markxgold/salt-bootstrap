e541b78f9c12697d6dc6943c1193dea8762cc2a0
Mark Gold <mark.gold@ezoka.net>
2019-12-27 12:05:22 +0000

    Made TLS 1.2 the first option

--- bootstrap-salt-int.ps1
+++ bootstrap-salt-int.ps1
@@ -92,7 +92,7 @@ Param(
 )
 
 # Powershell supports only TLS 1.0 by default. Add support up to TLS 1.2
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12,Tls11,Tls'
 
 #===============================================================================
 # Script Functions
